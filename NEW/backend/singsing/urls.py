f23fb270d9cc08315a43d9e72969165fc6979002
miiccc <imdamap4@gmail.com>
2025-10-13 22:55:02 +0800

    urls to view every html file

--- NEW/backend/singsing/urls.py
+++ NEW/backend/singsing/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("indepth/", views.indepth, name="indepth"),
+    path("promotions/", views.promotions, name="promotions"),
+    path("settings/", views.settings_view, name="settings"),
+    path("profile/", views.profile, name="profile"),
     path("products/", views.products, name="products"),
 ]
