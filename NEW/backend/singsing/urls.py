47d02a3c6586991a03d145b6f24fe82c7dfea3dc
miiccc <imdamap4@gmail.com>
2025-10-13 23:07:57 +0800

    changes to view the urls

--- NEW/backend/singsing/urls.py
+++ NEW/backend/singsing/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", include("singsing.urls"))
+urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
